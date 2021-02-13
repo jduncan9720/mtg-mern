c4abcfa26fcb245190717c545464cdbb2071832a
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2021-02-12 17:28:37 -0700

    fixing select

--- client/src/components/Artistlist/index.js
+++ client/src/components/Artistlist/index.js
@@ -1,19 +1,25 @@
 import React from 'react'
 
-export default function Artistlist(children) {
-    return (
-      <form>
+export default function Artistlist(props) {
+  console.log(props)
+  return (
+    <form>
       <label>
         Artist:
-        <select>{children}</select>
+        <select onChange={props.selectOption}>{props.artists.map(artist => (
+        <option 
+          key={artist._id}
+          name={artist.first_name}
+          value={artist._id}
+          > {artist.first_name} {artist.last_name}
+          </option>
+      ))}</select>
       </label>
-      <input type="submit" value="Submit" />
+      <input onSubmit={props.handleFormSubmit} type="submit" value="Submit" />
     </form>
-    )
+
+  )
 }
 
 
-export function Artistselect(children) {
-    return  <option>{children}</option>
-}
 
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,39 +1,50 @@
 import React, { useState, useEffect } from "react";
 import Artistlist from "../components/Artistlist"
-import Artistselect from "../components/Artistlist"
 import API from '../utils/API'
 
 function Admin() {
-const [artists, setArtists] = useState({})
+    const [artists, setArtists] = useState({})
+    const [selectedOpt, setSelectedOpt] = useState(0)
 
-  useEffect(() => {
-    loadArtists()
-  }, [])
+    useEffect(() => {
+        loadArtists()
+    }, [])
 
-  function loadArtists() {
-    API.getArtists()
-    .then(res =>
-      setArtists(res.data))
-  }
- 
-  if (artists.length > 0) {
-      console.log(artists)
-    return (
-        <Artistlist>  
-        {artists.map(artist => (
-            <Artistselect key={artist._id}>
-                {artist.first_name}
-            </Artistselect>
-        ))} 
-        </Artistlist>
-    )
-} else {
-    return (
-        <div>
-            <Artistlist />
-        </div>
-    )
-}
+    function loadArtists() {
+        API.getArtists()
+            .then(res =>
+                setArtists(res.data))
+    }
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        alert("working")
+
+    }
+
+    function selectOption(event){
+        const value = event.target.value
+        // const selected = opt.options[opt.selectedIndex].value
+        setSelectedOpt(value)
+        console.log(value)
+        console.log(selectedOpt)
+    }
+
+    if (artists.length > 0) {
+        console.log(artists)
+        return (
+            <Artistlist 
+                artists={artists} 
+                handleFormSubmit={handleFormSubmit} 
+                selectOption={selectOption}/>
+        )
+    } else {
+        return (
+            <div>
+                <Artistlist artists={[]}/>
+            </div>
+        )
+    }
 }
 export default Admin;
 
